e8b1872a9bf26d9eb72228b6ad7b792362140224
Timuottawa <tyuan@hotmail.com>
2020-08-07 07:56:38 -0400

    August 7

---
+++ Sense/Controller/OnBoardV.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoardViewController.swift
+//  Sense
+//
+//  Created by Lucinha7 Liu on 2020-08-04.
+//  Copyright © 2020 Bob Yuan. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnBoardV: PaperOnboarding {
+    
+    
+}
