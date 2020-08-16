026cd8fc7cc3e86b57df60919eda7dd3353b3f0b
Timuottawa <tyuan@hotmail.com>
2020-08-16 18:43:22 -0400

    ok

--- Sense/Global Classes/global.swift
+++ Sense/Global Classes/global.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Bob Yuan. All rights reserved.
 //
 
+import UIKit
 import Foundation
 import AVFoundation
 
@@ -49,6 +50,63 @@ class globalVariables {
             audioPlayer?.play()
   
         }
+    }
+}
+
+
+extension UIView {
+    func roundedTop(_ radius: CGFloat) {
+        let maskPath1 = UIBezierPath(roundedRect: bounds,
+                                     byRoundingCorners: [.topRight, .topLeft],
+                                     cornerRadii: CGSize(width: radius, height: radius))
+        let maskLayer1 = CAShapeLayer()
+        maskLayer1.frame = bounds
+        maskLayer1.path = maskPath1.cgPath
+        layer.mask = maskLayer1
+    }
+    
+    func dropShadow(shadowColor: UIColor = UIColor.black,
+                    fillColor: UIColor = UIColor.white,
+                    opacity: Float = 0.2,
+                    offset: CGSize = CGSize(width: 0.0, height: 5.0),
+                    radius: CGFloat = 10) -> CAShapeLayer {
+        
+        let shadowLayer = CAShapeLayer()
+        
+        shadowLayer.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: radius).cgPath
+        shadowLayer.fillColor = fillColor.cgColor
+        shadowLayer.shadowColor = shadowColor.cgColor
+        shadowLayer.shadowPath = shadowLayer.path
+        shadowLayer.shadowOffset = offset
+        shadowLayer.shadowOpacity = opacity
+        shadowLayer.shadowRadius = radius
+        layer.insertSublayer(shadowLayer, at: 0)
+        return shadowLayer
+    }
+}
+
+// Defined by Tim on August 6, 2020
+public var _screenWidth: CGFloat {
+    return UIScreen.main.bounds.width
+}
+public var _screenHeight: CGFloat {
+    return UIScreen.main.bounds.height
+}
+
+extension UIViewController {
+    func popupAlert(msg: String) {
+        let alert = UIAlertController(title: "", message: msg, preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "OK", style: .default) {
+            (action: UIAlertAction) in
+            print("OK")
+        }
+        let action2 = UIAlertAction(title: "Cancel", style: .cancel) {
+            (action: UIAlertAction) in
+            print("Cancel")
+        }
+        //alert.addAction(action1)
+        alert.addAction(action1)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
